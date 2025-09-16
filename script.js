78c6ad0bed9b3f76598bfdd6da3d6649340fd2db
Khayyat <fadialkhayyat@outlook.com>
2025-09-16 20:26:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -173,7 +173,6 @@ window.onload = () => {
     };
 
     window.addEventListener("mousemove", handleInteraction);
-    // Use touchstart for mobile to avoid interfering with scrolling
     hero.addEventListener("touchstart", handleInteraction);
 
     // Reset tilt on mouse/touch leave
